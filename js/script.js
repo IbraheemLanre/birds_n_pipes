28b6abffcabf0b9862a13ed60df37a6fb53f37ef
IbraheemLanre <s2001108@edu.bc.fi>
2020-09-27 15:36:43 +0300

    Added bird class

--- js/script.js
+++ js/script.js
@@ -56,10 +56,50 @@ class Pipe {
   }
 }
 
+class Bird {
+  constructor(parentEl) {
+    this.id = "bird_" + Math.floor(Math.random() * 2000);
+    this.imgSrc = "../img/bird.png";
+    this.style = {
+      position: "fixed",
+      top: Math.floor(Math.random() * 70) + "%",
+      left: Math.floor(Math.random() * 2000) + "px",
+      width: Math.floor(Math.random() * 200) + "px",
+    };
+    this.movementRatio = 20;
+
+    var birdEl = document.createElement("img");
+    birdEl.src = this.imgSrc;
+    birdEl.id = this.id;
+    Object.assign(birdEl.style, this.style);
+
+    parentEl.appendChild(birdEl);
+  }
+
+  moveLeft(distance) {
+    let _current = parseInt(this.style.left);
+    document.getElementById(this.id).style.left = `${
+      _current + distance / this.movementRatio
+    }px`;
+  }
+}
+
 $(document).ready(function () {
   // instance of the class Background, "this" variable inside an instance refers to the instance itself
   // not to the class
   var bg = new Background(document.getElementById("background"));
+
+  var pipe1 = new Pipe(document.body);
+  var pipe2 = new Pipe(document.body);
+  var pipe3 = new Pipe(document.body);
+  var pipe4 = new Pipe(document.body);
+  var pipe5 = new Pipe(document.body);
+
+  var bird1 = new Bird(document.body);
+  var bird2 = new Bird(document.body);
+  var bird3 = new Bird(document.body);
+  var bird4 = new Bird(document.body);
+  var bird5 = new Bird(document.body);
 
   document.addEventListener("scroll", function () {
     var offset = window.scrollY;
@@ -70,10 +110,11 @@ $(document).ready(function () {
     pipe3.moveLeft(offset);
     pipe4.moveLeft(offset);
     pipe5.moveLeft(offset);
+
+    bird1.moveLeft(offset);
+    bird2.moveLeft(offset);
+    bird3.moveLeft(offset);
+    bird4.moveLeft(offset);
+    bird5.moveLeft(offset);
   });
-  var pipe1 = new Pipe(document.body);
-  var pipe2 = new Pipe(document.body);
-  var pipe3 = new Pipe(document.body);
-  var pipe4 = new Pipe(document.body);
-  var pipe5 = new Pipe(document.body);
 });
