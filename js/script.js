003826e831ba36be30f06f9e3826835f93d360bf
IbraheemLanre <s2001108@edu.bc.fi>
2020-09-27 14:57:32 +0300

    modified the pipe and background movement

--- js/script.js
+++ js/script.js
@@ -1,63 +1,79 @@
 console.log("Loading...");
 
-var infiniteBackground = {
-  el: null,
-  style: {
-    top: 0,
-    left: 0,
-    position: "fixed",
-    backgroundImage: null,
-    backgroundRepeat: "repeat-x",
-    backgroundPosition: "0 0",
-    backgroundSize: "auto 100%",
-    height: "100%",
-    width: "100%",
-  },
-  scrollSideways: function () {
-    var scope = this;
-    document.addEventListener("scroll", function () {
-      var topOffset = $(scope.el).offset().top;
-      scope.el.style.backgroundPosition = `-${topOffset}px 0px`;
-    });
-  },
-  init: function (el, bg = "../img/bg.png") {
+class Background {
+  constructor(el, bgImg = "../img/bg.png") {
     this.el = el;
-    this.style.backgroundImage = `url("${bg}")`;
+    this.style = {
+      left: 0,
+      top: 0,
+      position: "fixed",
+      height: "100%",
+      width: "100%",
+      backgroundRepeat: "repeat-x",
+      backgroundPosition: "0 0",
+      backgroundSize: "auto 100%",
+      backgroundImage: `url("${bgImg}")`,
+    };
+    this.movementRatio = 50;
     Object.assign(this.el.style, this.style);
-    this.scrollSideways();
-  },
-};
+  }
 
-var pipe = {
-  //definition of object of the class pipe
-  imgSrc: "../img/pipe.png",
-  size: null,
-  position: {
-    top: null,
-    left: null,
-  },
-  style: {
-    position: "fixed",
-    top: null,
-    left: null,
-    width: null,
-    bottom: 0,
-  },
-  flip: false,
-  init: function (parentEl) {
+  scrollSideWay(distance) {
+    this.el.style.backgroundPosition = `-${
+      distance / this.movementRatio
+    }px 0px`;
+  }
+}
+
+class Pipe {
+  constructor(parentEl) {
+    this.id = "pipe_" + Math.floor(Math.random() * 2000);
+    this.imgSrc = "../img/pipe.png";
+    this.size = null;
+    this.style = {
+      position: "fixed",
+      top: null,
+      left: Math.floor(Math.random() * 2000) + "px",
+      width: Math.floor(Math.random() * 200) + "px",
+      bottom: 0,
+    };
+    this.flip = false;
+    this.movementRatio = 20;
+
     var pipeEl = document.createElement("img");
     pipeEl.src = this.imgSrc;
-    this.style.left = Math.floor(Math.random() * 1000) + "px";
-    this.style.width = Math.floor(Math.random() * 200) + "px";
+    pipeEl.id = this.id;
     Object.assign(pipeEl.style, this.style);
+
     parentEl.appendChild(pipeEl);
-  },
-};
+  }
+
+  moveLeft(distance) {
+    let _current = parseInt(this.style.left);
+    document.getElementById(this.id).style.left = `${
+      _current - distance / this.movementRatio
+    }px`;
+  }
+}
 
 $(document).ready(function () {
-  infiniteBackground.init(document.getElementById("overlay"));
-  var pipe1 = pipe;
-  var pipe2 = pipe;
-  pipe1.init(document.body);
-  pipe2.init(document.body);
+  // instance of the class Background, "this" variable inside an instance refers to the instance itself
+  // not to the class
+  var bg = new Background(document.getElementById("background"));
+
+  document.addEventListener("scroll", function () {
+    var offset = window.scrollY;
+
+    bg.scrollSideWay(offset);
+    pipe1.moveLeft(offset);
+    pipe2.moveLeft(offset);
+    pipe3.moveLeft(offset);
+    pipe4.moveLeft(offset);
+    pipe5.moveLeft(offset);
+  });
+  var pipe1 = new Pipe(document.body);
+  var pipe2 = new Pipe(document.body);
+  var pipe3 = new Pipe(document.body);
+  var pipe4 = new Pipe(document.body);
+  var pipe5 = new Pipe(document.body);
 });
